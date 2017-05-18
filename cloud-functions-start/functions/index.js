b67d00c50d1af9c607dba4fa97852801cd0de42d
balazs4 <balazs4web@gmail.com>
2017-05-18 22:56:01 +0200

    google vision (billing)

--- cloud-functions-start/functions/index.js
+++ cloud-functions-start/functions/index.js
@@ -1,7 +1,43 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const gcs = require('@google-cloud/storage')();
+const vision = require('@google-cloud/vision')();
+const { exec } = require('child-process-promise');
 
 admin.initializeApp(functions.config().firebase);
+
+exports.blurBadImages = functions.storage.object().onChange(({ data }) => {
+  const bucket = gcs.bucket(data.bucket);
+  const file = bucket.file(data.name);
+  return vision.detectSafeSearch(file).then(([{ adult, violence }]) => {
+    if (!adult) {
+      console.log('adult check', 'passed');
+      return;
+    }
+
+    if (!violence) {
+      console.log('violence check', 'passed');
+      return;
+    }
+
+    console.log('Bad image');
+    return blurImage(data.name, bucket);
+  });
+});
+
+const blurImage = (filepath, bucket) => {
+  return bucket
+    .file(filepath)
+    .download({ destination: '/tmp/foobar' })
+    .then(() =>
+      exec('convert' + tmpfile + ' -channel RGBA -blur 0x24' + tmpfile)
+    )
+    .then(() => bucket.upload('/tmp/foobar', { destination: filepath }))
+    .then(() =>
+      admin.database().ref('/messages/foobar').update({ moderated: true })
+    )
+    .then(() => console.log('DONE'));
+};
 
 exports.addWelcomeMessage = functions.auth.user().onCreate(event => {
   console.log(JSON.stringify(event));
@@ -17,9 +53,9 @@ exports.sendNotification = functions.database
   .ref('/messages/{messageId}')
   .onWrite(({ data }) => {
     if (data.previous.val()) {
-        console.log(JSON.stringify(data.previous.key))
-        return;
-    };
+      console.log(JSON.stringify(data.previous.key));
+      return;
+    }
 
     const { name, text, photoUrl } = data.val();
     const payload = {
