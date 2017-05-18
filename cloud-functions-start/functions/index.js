cdcf5e3beeb9b4779fafb2b76f554fa3a56fcb5d
balazs4 <balazs4web@gmail.com>
2017-05-18 22:26:55 +0200

    before vision

--- cloud-functions-start/functions/index.js
+++ cloud-functions-start/functions/index.js
@@ -7,8 +7,36 @@ exports.addWelcomeMessage = functions.auth.user().onCreate(event => {
   console.log(JSON.stringify(event));
 
   return admin.database().ref('messages').push({
-      name: 'Foo Bar',
-      photoUrl: '/images/firebase-logo.png',
-      text: `${event.data.displayName || 'Unknown display name'} has joined the chat!`
-  })
+    name: 'Foo Bar',
+    photoUrl: '/images/firebase-logo.png',
+    text: `${event.data.displayName || 'Unknown display name'} has joined the chat!`
+  });
 });
+
+exports.sendNotification = functions.database
+  .ref('/messages/{messageId}')
+  .onWrite(({ data }) => {
+    if (data.previous.val()) {
+        console.log(JSON.stringify(data.previous.key))
+        return;
+    };
+
+    const { name, text, photoUrl } = data.val();
+    const payload = {
+      notification: {
+        title: `${name} said something`,
+        body: text || '',
+        icon: photoUrl || '/images/profile_placeholder.png',
+        click_action: `https://${functions.config().firebase.authDomain}`
+      }
+    };
+
+    console.log(payload);
+
+    return admin.database().ref('fcmTokens').once('value').then(alltokens => {
+      if (!alltokens.val()) return;
+      return admin
+        .messaging()
+        .sendToDevice(Object.keys(alltokens.val()), payload);
+    });
+  });
