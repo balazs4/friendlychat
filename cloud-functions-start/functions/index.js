ff56c3047107040c9cca13940934412b9e54403f
balazs4 <balazs4web@gmail.com>
2017-05-18 21:35:15 +0200

    push log entry into db

--- cloud-functions-start/functions/index.js
+++ cloud-functions-start/functions/index.js
@@ -4,5 +4,11 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.addWelcomeMessage = functions.auth.user().onCreate(event => {
-  console.log(event);
+  console.log(JSON.stringify(event));
+
+  return admin.database().ref('messages').push({
+      name: 'Foo Bar',
+      photoUrl: '/images/firebase-logo.png',
+      text: `${event.data.displayName || 'Unknown display name'} has joined the chat!`
+  })
 });
